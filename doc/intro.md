dae64b1ffa3f0c4d9080827b20234a50dd0d669f
Dave Aitken <dave.aitken@gmail.com>
2013-08-11 20:07:15 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-boids
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clojure_boids/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-boids.core-test
+  (:use clojure.test
+        clojure-boids.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
