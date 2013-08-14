44d9255f689a5fce71599f19c90b795ada356bdf
Dave Aitken <dave.aitken@gmail.com>
2013-08-14 23:05:04 +0100

    Working openGL display with lwjgl

--- project.clj
+++ project.clj
@@ -1,6 +1,19 @@
+(require 'leiningen.core.eval)
+
+(def lwjgl-native-classifier 
+  (let [classifiers {:macosx "natives-osx"
+                     :linux "natives-linux"
+                     :windows "natives-windows"}
+        os (leiningen.core.eval/get-os)]
+    (get classifiers os)))
+
 (defproject clojure-boids "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Boids"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.lwjgl.lwjgl/lwjgl "2.9.0"]
+                 [org.lwjgl.lwjgl/lwjgl-platform "2.9.0"
+                  :classifier ~lwjgl-native-classifier :native-prefix ""]]
+  :main clojure-boids.core)
--- src/clojure_boids/core.clj
+++ src/clojure_boids/core.clj
@@ -1,6 +1,24 @@
-(ns clojure-boids.core)
+(ns clojure-boids.core
+  (:import [org.lwjgl.opengl Display DisplayMode]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn init [] 
+  (let [display-mode (DisplayMode. 800 600)]
+    (Display/setDisplayMode display-mode)
+    (Display/create)))
+
+(defn draw [] ())
+
+(defn cleanup []
+  (Display/destroy)
+  (prn "Bye!"))
+
+(defn main-loop []
+  (if (not (Display/isCloseRequested))
+    (do
+      (draw)
+      (recur))
+    (cleanup)))
+
+(defn -main []
+  (init)
+  (.start (Thread. main-loop)))
