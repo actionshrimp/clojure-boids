824c8767c042f28b3413de7dc7be878015a2cae3
Dave Aitken <dave.aitken@gmail.com>
2013-08-17 16:01:40 +0100

    A bit of swarming

--- src/clojure_boids/boid.clj
+++ src/clojure_boids/boid.clj
@@ -1,24 +1,53 @@
 (ns clojure-boids.boid
   (:import [java.lang.Math]))
 
-(def velocity 1)
+(def v-mag 0.1)
+(def w-max-mag 0.005)
 
 (defn random [world-w world-h n]
-  (let [m 50
-        w (- world-w (* 2 m))
-        h (- world-h (* 2 m))]
-  {:x (+ (rand w) m) 
-   :y (+ (rand h) m) 
-   :a (rand (* 2 Math/PI))
-   :w 0
-   :alpha 0
-   }))
+  (let [world-margin 50
+        w (- world-w (* 2 world-margin))
+        h (- world-h (* 2 world-margin))
+        a (rand (* 2 Math/PI))]
+  {:s [(+ (rand w) world-margin) 
+       (+ (rand h) world-margin)] 
+   :a a
+   :v [(Math/sin a) (Math/cos a)] }))
 
-(defn update [dt world {:keys [x y a w alpha] :as boid}]
-  (let [v-x (Math/sin a) 
-        v-y (Math/cos a)]
-    (assoc boid 
-           :x (+ x (* dt v-x))
-           :y (+ y (* dt v-y))
-           :a (+ a (* dt w))
-           :w (+ w (* dt alpha)))))
+(defn mag-bounded [max-mag value]
+  (max (- max-mag) (min max-mag value)))
+
+(defn dot [v1 v2]
+  (apply + (doall (map * v1 v2))))
+
+(defn normalize [v]
+  (let [mag (Math/sqrt (dot v v))]
+    (doall (map #(/ % mag) v))))
+
+(defn calculate-w [t {:keys [s v a]}]
+  (let [relative-t (doall (map - t s))
+        target-v (normalize relative-t)
+        w-mag (mag-bounded w-max-mag (* -1 (- (dot v target-v) 1)))
+        steer-right-vec [(second v) (- (first v))]
+        delta-v (doall (map - target-v v))
+        w-direc (dot delta-v steer-right-vec)]
+    (if (> w-direc 0)
+      w-mag
+      (- w-mag))))
+
+(defn update [dt world {:keys [s a] :as boid}]
+  (let [target-s [400 400] 
+        w (calculate-w target-s boid)
+        new-a (+ a (* dt w))
+        new-v-unit [(Math/sin new-a) (Math/cos new-a)]
+        ds (doall (map #(* dt v-mag %) new-v-unit))
+        new-s (doall (map + s ds))]
+    (assoc boid
+           :s new-s
+           :a new-a
+           :v new-v-unit)))
+
+;The distance from an obstacle a boid needs to start turning 
+;in order to avoid it if aiming straight at it
+(def panic-distance 
+  ())
--- src/clojure_boids/graphics/core.clj
+++ src/clojure_boids/graphics/core.clj
@@ -11,17 +11,18 @@
 (defn clear []
   (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT GL11/GL_DEPTH_BUFFER_BIT)))
 
-(defn draw-boid [{:keys [x y a]}]
-  (GL11/glPushMatrix)
-  (GL11/glTranslatef x y 0)
-  (GL11/glRotatef (Math/toDegrees a) 0 0 -1)
-  (GL11/glBegin GL11/GL_TRIANGLES)
-  (GL11/glColor3f 0.5 0.5 1.0)
-  (GL11/glVertex2f 0 0)
-  (GL11/glVertex2f 10 0)
-  (GL11/glVertex2f 5 10)
-  (GL11/glEnd)
-  (GL11/glPopMatrix))
+(defn draw-boid [{:keys [s a]}]
+  (let [[x y] s]
+    (GL11/glPushMatrix)
+    (GL11/glTranslatef x y 0)
+    (GL11/glRotatef (Math/toDegrees a) 0 0 -1)
+    (GL11/glBegin GL11/GL_TRIANGLES)
+    (GL11/glColor3f 0.5 0.5 1.0)
+    (GL11/glVertex2f 0 0)
+    (GL11/glVertex2f 10 0)
+    (GL11/glVertex2f 5 10)
+    (GL11/glEnd)
+    (GL11/glPopMatrix)))
 
 (defn draw [world]
   (clear)
