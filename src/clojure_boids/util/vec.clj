3cbd5e646af325fe8123b8f5b40842281b956062
Dave Aitken <dave.aitken@gmail.com>
2013-08-18 00:56:59 +0100

    Add util vec

---
+++ src/clojure_boids/util/vec.clj
@@ -0,0 +1,22 @@
+(ns clojure-boids.util.vec)
+
+(defn dot [v1 v2]
+  (apply + (doall (map * v1 v2))))
+
+(defn normalize [v]
+  (let [mag (Math/sqrt (dot v v))]
+    (doall (map #(/ % mag) v))))
+
+(defn mag-bounded [max-mag v]
+  (let [mag (Math/sqrt (dot v v))]
+    (if (> mag max-mag)
+      (normalize v) v)))
+
+(defn add [v1 v2]
+  (doall (map + v1 v2)))
+
+(defn sub [v1 v2]
+  (doall (map - v1 v2)))
+
+(defn scale [s v1]
+  (doall (map #(* s %) v1)))
